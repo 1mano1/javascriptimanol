f0d1f78adb6543d861ba8a1c0badc25362492504
1mano1 <orodriguez27@ucol.mx>
2023-10-04 11:32:51 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Declaration de variables de javascript modern o
+// let numero = 5;
+// numero = 8;
+// const constante = 4;
+// constante = 10;
+
+//Alerta en javascript
+
+// alert('Hola desde una alerta de javascript');
+//         console.log('Esto es Javascrip desde el HTML');
+//         console.log(numero);
+//         console.log(constante);
+
+//Concatenación de javascript
+let nombre = 'Imanol';
+let apellido = ' Rodriguez';
+
+console.log('Mi Nombre es: ' + nombre + apellido);
+
+// document.write('Hola en el documento de javascript');
+
+let selection = document.querySelector('#section1');
+
+// console.log(selection);
+
+selection.innerHTML = `Mi nombre es Imanol Rodríguez`;
+// document.write('Mi Nombre es: ' + nombre + apellido);
